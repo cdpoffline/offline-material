e11cc970242c42658096d2fb266529127b49e7db
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-01-27 19:09:19 +0000

    added link to offline materials to all web fodlers

--- bin/link_to_web_folder.sh
+++ bin/link_to_web_folder.sh
@@ -2,24 +2,29 @@
 
 # get the file name 
 #   http://www.cyberciti.biz/faq/unix-linux-extract-filename-and-extension-in-bash/
-web_folder="$1"
+offline_materials_web_folder_path="`realpath \"$1\"`"
 
-if [ -z "$web_folder" ]
+if [ -z "$offline_materials_web_folder_path" ]
 then
   >&2 echo "ERROR: The first argument must be the web folder."
   exit 1
 fi
 
-folder="`pwd`"
-web_folder_name="${folder##*/}"
+working_directory="`pwd`"
+module_name="${working_directory##*/}"
 
-for possible_web_folder_name in _site web
+for module_web_folder in _site web
 do
-  full_path="$folder/$possible_web_folder_name"
-  web_path="$web_folder/$web_folder_name"
-  if [ -d "$full_path" ]
+  module_web_folder_path="$working_directory/$module_web_folder"
+  module_web_folder_path_in_offline_materials="$offline_materials_web_folder_path/$module_name"
+  link_to_offline_materials_web_folder="$module_web_folder_path/offline-materials"
+  if [ -d "$module_web_folder_path" ]
   then
-    rm -f $web_path && ln -s "$full_path" "$web_path" 
+    rm -f "$module_web_folder_path_in_offline_materials"
+    ln -s -T "$module_web_folder_path" "$module_web_folder_path_in_offline_materials"
+    
+    rm -f "$link_to_offline_materials_web_folder"
+    ln -s -T "$offline_materials_web_folder_path" "$link_to_offline_materials_web_folder"
     break
   fi
 done
