e05af621201c083ff7ccf9a5a8b185089dd92715
Nicco Kunzmann <niccokunzmann@googlemail.com>
2017-02-07 10:16:02 -0800

    removed ssh clone

--- bin/download_and_install.sh
+++ bin/download_and_install.sh
@@ -7,7 +7,7 @@ sudo apt-get -y -q install git
 mkdir -p "$directory"
 cd $directory
 
-git clone git@github.com:cdpoffline/offline-material.git . || git clone https://github.com/cdpoffline/offline-material.git .
+git clone https://github.com/cdpoffline/offline-material.git .
 
 cd bin
 
