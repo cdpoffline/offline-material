1642cda8a345c8ed122e92d74e95e4378e353af2
Nicco Kunzmann <niccokunzmann@googlemail.com>
2017-02-07 10:18:10 -0800

    accepting authenticity of github

--- bin/download_and_install.sh
+++ bin/download_and_install.sh
@@ -7,7 +7,7 @@ sudo apt-get -y -q install git
 mkdir -p "$directory"
 cd $directory
 
-git clone https://github.com/cdpoffline/offline-material.git .
+echo "yes" | git clone https://github.com/cdpoffline/offline-material.git .
 
 cd bin
 
